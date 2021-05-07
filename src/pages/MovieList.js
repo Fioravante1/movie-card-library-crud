b09288505691e13f43ebc5e850407cef49a708a6
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-07 16:55:37 -0300

    Resoluçao do requisito dois

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
-import * as movieAPI from '../services/movieAPI';
+import { getMovies } from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -12,10 +13,18 @@ class MovieList extends Component {
     };
   }
 
+  componentDidMount() {
+    getMovies().then((movies) => {
+      this.setState({ movies });
+    });
+  }
+
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
+    if (movies.length === 0) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-list">
