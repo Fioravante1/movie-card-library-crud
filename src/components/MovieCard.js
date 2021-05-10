b0965aba0b77691ab499d65ab3ffdce3322af59f
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-10 16:10:23 -0300

    desetruturação de image, genre e subtlite

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, id } = movie;
+    const { title, storyline, id, imagePath, genre, subtitle } = movie;
     const URL = `/movies/${id}`;
     return (
       <div data-testid="movie-card">
