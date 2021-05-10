fec01bd47624589a284abdf91c80fea153634076
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-10 16:11:42 -0300

    Inclui genero do filme

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -14,6 +14,7 @@ class MovieCard extends React.Component {
         <h1>{ title }</h1>
         <h3>{ subtitle }</h3>
         <p>{ storyline }</p>
+        <p>{ genre }</p>
         <Link to={ URL }>VER DETALHES</Link>
       </div>
     );
